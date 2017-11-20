492574cbb9b50b20db27057afa2e33116d7f7d56
Erick Borsboom <erick.borsboom@ribose.com>
2017-11-20 11:51:13 +0800

    minor path fixes

--- openbsd/install-jenkins.sh
+++ openbsd/install-jenkins.sh
@@ -22,7 +22,7 @@ config = "/etc/adduser.conf"
 logfile = "/var/log/adduser"
 home = "/home"
 path = ('/bin', '/usr/bin', '/usr/local/bin')
-shellpref = (''sh', 'ksh', 'nologin')
+shellpref = ('sh', 'ksh', 'nologin')
 defaultshell = "sh"
 defaultgroup = "USER"
 uid_start = 1000
@@ -37,13 +37,13 @@ pkg_add -Uu
 pkg_add jre bash
 ln -s /usr/local/jre-1.8.0/bin/java /usr/bin
 ln -s /usr/local/bin/bash /bin
-pkg_add gmake libtool libevent autoconf-2.69p2 automake-1.15p0 python-3.5.2p2 llvm boost git
+pkg_add gmake libtool libevent autoconf-2.69p2 automake-1.15.1 python-3.6.2 llvm boost git
 
 curl --create-dirs -sSLo ${JENKINS_SLAVE_PATH} ${JENKINS_SLAVE_URL} \
 && chmod 755 `dirname ${JENKINS_SLAVE_PATH}` && chmod 644 ${JENKINS_SLAVE_PATH}
 
 echo "$JENKINS_SLAVE_SHA  ${JENKINS_SLAVE_PATH}" | sha1 -c -
-if [ $? -ne 0 ]; then exit 1 ; fi
+if [ $? -ne 0 ]; then exit 1; fi
 
 # Create jenkins start script
 cat > /usr/local/scripts/safe_jenkins << EOF
